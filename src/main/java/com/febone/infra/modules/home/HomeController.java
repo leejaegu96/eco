de0fe6d2a32a2588dd3d8a56b376f4498882235d
zzjiho <youngyoungnim@gmail.com>
2023-02-19 17:18:08 +0900

    homeController

--- src/main/java/com/febone/infra/modules/home/HomeController.java
+++ src/main/java/com/febone/infra/modules/home/HomeController.java
@@ -1,4 +1,4 @@
-package com.febone.infra;
+package com.febone.infra.modules.home;
 
 import java.util.Locale;
 
@@ -7,13 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class HomeController {
-	
-	@RequestMapping(value="", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		return "home";
-	}
-	
+   @RequestMapping(value="", method = RequestMethod.GET)
+   public String mainForm(Locale locale, Model model) {
+      return "info/loginForm";
+   }
 }
